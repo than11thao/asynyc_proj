6b700225160d8099a46a345f7e3732ed765ee0f8
Than Thao <thanthao051102@gmail.com>
2023-06-06 09:38:23 +0700

    first commit

---
+++ async_proj/views.py
@@ -0,0 +1,61 @@
+from django.http import HttpResponse
+import time,asyncio
+from movies.models import Movie
+from stories.models import Story
+from asgiref.sync import sync_to_async
+
+#helper funcs 
+def get_movies():
+    print('prepare to get the movies....')
+    time.sleep(2)
+    qs = Movie.objects.all()
+    print(qs)
+    print('got all the movies!')
+
+def get_stories():
+    print('prepare to get the stories....')
+    time.sleep(5)
+    qs = Story.objects.all()
+    print(qs)
+    print('got all the sotries!')
+
+@sync_to_async
+def get_movies_async():
+    print('prepare to get the movies....')
+    time.sleep(2)
+    qs = Movie.objects.all()
+    print(qs)
+    print('got all the movies!')
+
+
+@sync_to_async
+def get_stories_async():
+    print('prepare to get the stories....')
+    time.sleep(5)
+    qs = Story.objects.all()
+    print(qs)
+    print('got all the sotries!')
+
+
+#view
+def home_view(request):
+    return HttpResponse("Hello")
+
+def main_view(request):
+    start_time = time.time()
+    get_movies()
+    get_stories()
+    total = (time.time()-start_time)
+    print('total:' ,total)
+    return HttpResponse('sync')
+
+async def main_view_async(request):
+    start_time = time.time()
+    # task1 = asyncio.ensure_future(get_movies_async())
+    # task2 = asyncio.ensure_future(get_stories_async())
+    # await asyncio.wait([task1,task2])
+    await asyncio.gather(get_movies_async(),get_stories_async())
+    total = (time.time()-start_time)
+    print('total:' ,total)
+    return HttpResponse('async')
+
